4712a6575fc2e6a5368f551c9c4f90bf807adf74
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-02 23:17:51 +0700

    check score only process high trust

--- dataColection.py
+++ dataColection.py
@@ -2,6 +2,10 @@ import cvzone
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2
 
+confidence = 0.8
+
+blurThreshold = 39
+
 offsetPercentageW = 10
 offsetPercentageH = 20
 camW, camH = 640, 480
@@ -28,44 +32,45 @@ while True:
             score = bbox["score"][0]
             # print(x, y, w, h)
 
-            # ----------------- add offset --------------------
-            offsetW = offsetPercentageW / 100 * w
-            x = x - int(offsetW)
-            w = w + int(offsetW * 2)
+            # ----------------- check score--------------------
+            if score >= confidence:
 
-            offsetH = offsetPercentageH / 100 * h
-            y = y - int(offsetH * 3)
-            h = h + int(offsetH * 3.5)
+                # ----------------- add offset --------------------
+                offsetW = offsetPercentageW / 100 * w
+                x = x - int(offsetW)
+                w = w + int(offsetW * 2)
 
-            # ---------------- avoid below 0 -----------------
-            if x < 0: x = 0
-            if y < 0: y = 0
-            if w < 0: w = 0
-            if h < 0: h = 0
+                offsetH = offsetPercentageH / 100 * h
+                y = y - int(offsetH * 3)
+                h = h + int(offsetH * 3.5)
 
-            # --------- extract blur  noice cancel------------
-            imgFace = img[y:y + h, x:x + w]
-            cv2.imshow("face", imgFace)
-            blurValue = cv2.Laplacian(imgFace, cv2.CV_64F).var()
+                # ---------------- avoid below 0 -----------------
+                if x < 0: x = 0
+                if y < 0: y = 0
+                if w < 0: w = 0
+                if h < 0: h = 0
 
-            # ---------------- Normalize Value -----------------
-            ih, iw, _ = img.shape
-            xc, yc = x + w / 2, y + h / 2
-            xcn, ycn = round(xc / iw, floatingNumber), round(yc / ih, floatingNumber)
-            wn, hn = round(w / iw, floatingNumber), round(h / ih, floatingNumber)
+                # --------- extract blur  noice cancel------------
+                imgFace = img[y:y + h, x:x + w]
+                cv2.imshow("face", imgFace)
+                blurValue = cv2.Laplacian(imgFace, cv2.CV_64F).var()
 
-            # ---------------- avoid above 1 -----------------
-            if xcn > 1: xcn = 1
-            if ycn > 1: ycn = 1
-            if wn > 1: wn = 1
-            if hn > 1: hn = 1
+                # ---------------- Normalize Value -----------------
+                ih, iw, _ = img.shape
+                xc, yc = x + w / 2, y + h / 2
+                xcn, ycn = round(xc / iw, floatingNumber), round(yc / ih, floatingNumber)
+                wn, hn = round(w / iw, floatingNumber), round(h / ih, floatingNumber)
 
-            # ---------------- draw -----------------
-            cv2.rectangle(img, (x, y, w, h), (255, 0, 0), 3)
+                # ---------------- avoid above 1 -----------------
+                if xcn > 1: xcn = 1
+                if ycn > 1: ycn = 1
+                if wn > 1: wn = 1
+                if hn > 1: hn = 1
 
-    # if save:
-    #     if all(listBlur) and listBlur!=[]:
-    #
+                # ---------------- draw -----------------
+                cv2.rectangle(img, (x, y, w, h), (255, 0, 0), 3)
+                cvzone.putTextRect(img, f'Score: {int(score * 100)}% Blur: {blurValue}', (x, y - 20),
+                                   scale=1, thickness=1)
 
-cv2.imshow("Image", img)
-cv2.waitKey(1)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
