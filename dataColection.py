cbcdcbb0e31baee1603e63efc706e3188f917ae9
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-02 23:14:29 +0700

    cut face out only

--- dataColection.py
+++ dataColection.py
@@ -2,6 +2,7 @@ import cvzone
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2
 
+
 offsetPercentageW = 10
 offsetPercentageH = 20
 camW, camH = 640, 480
@@ -28,23 +29,35 @@ while True:
             score = bbox["score"][0]
             # print(x, y, w, h)
 
-            # ----------------- add offset --------------------
-            offsetW = offsetPercentageW / 100 * w
-            x = x - int(offsetW)
-            w = w + int(offsetW * 2)
 
-            offsetH = offsetPercentageH / 100 * h
-            y = y - int(offsetH * 3)
-            h = h + int(offsetH * 3.5)
+                # ----------------- add offset --------------------
+                offsetW = offsetPercentageW / 100 * w
+                x = x - int(offsetW)
+                w = w + int(offsetW * 2)
 
-            # ---------------- avoid below 0 -----------------
-            if x < 0: x = 0
-            if y < 0: y = 0
-            if w < 0: w = 0
-            if h < 0: h = 0
+                offsetH = offsetPercentageH / 100 * h
+                y = y - int(offsetH * 3)
+                h = h + int(offsetH * 3.5)
 
-            # ---------------- draw -----------------
-            cv2.rectangle(img, (x, y, w, h), (255, 0, 0), 3)
+                # ---------------- avoid below 0 -----------------
+                if x < 0: x = 0
+                if y < 0: y = 0
+                if w < 0: w = 0
+                if h < 0: h = 0
+
+                # --------- extract blur  noice cancel------------
+                imgFace = img[y:y + h, x:x + w]
+                cv2.imshow("face", imgFace)
+                blurValue = cv2.Laplacian(imgFace, cv2.CV_64F).var()
+
+
+
+
+
+
+                # ---------------- draw -----------------
+                cv2.rectangle(img, (x, y, w, h), (255, 0, 0), 3)
+
 
         # if save:
         #     if all(listBlur) and listBlur!=[]:
